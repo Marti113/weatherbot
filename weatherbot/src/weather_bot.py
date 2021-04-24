708e54d6dbe18f126c8a0db93dd0e211ca56177d
Marti Kandallu <marti.kandallu@gmail.com>
2021-04-24 13:10:58 -0700

    Working code if manual input

--- weatherbot/src/weather_bot.py
+++ weatherbot/src/weather_bot.py
@@ -25,29 +25,86 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-def capture_user_city():
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def reply_weather():
+
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
 
     mentions = api.mentions_timeline(
-    tweet_mode='extended'
-    )
+                last_seen_id,
+                tweet_mode='extended')
 
     for mention in reversed(mentions):
-        if '#hike' in mention.full_text.lower():
-            city_name = mention.full_text.split('#hike')
-            # needs to be updated for user input
-            #city_name = 'Asheville'
-            api_address = 'https://api.openweathermap.org/data/2.5/weather?q=' + city_name + '&appid=62c7ceaaad980045d006ea4704872c79'
+        print(str(mention.id) + "-"+ mention.full_text)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
 
-            json_data = requests.get(api_address).json()
-            formatted_data = json_data['weather'][0]['description']
-            print(formatted_data)
+        if '#weather' in mention.full_text.lower():
+            print('found the tweet')
+            city_name = 'Portland'
             try:
-                api.update_status('@' + mention.user.screen_name + ' The weather is '+ formatted_data, mention.id)
+                # needs to be updated for user input
+
+                api_address = 'https://api.openweathermap.org/data/2.5/weather?q=' + city_name + '&appid=62c7ceaaad980045d006ea4704872c79'
+                json_data = requests.get(api_address).json()
+                formatted_data = json_data['weather'][0]['description']
+                api.update_status('@' + mention.user.screen_name + ' The weather is ' + formatted_data, mention.id)
+
             except tweepy.TweepError as error:
                 print("I don't know that city, try again", error)
         else:
-            api.update_status('@' + mention.user.screen_name + ' I need a #hike to find the city', mention.id)
+            print("You done messed up")
+            #api.update_status('@' + mention.user.screen_name + ' I need a #hike to find the city', mention.id)
 
 while True:
-    capture_user_city()
+    reply_weather()
     time.sleep(15)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
